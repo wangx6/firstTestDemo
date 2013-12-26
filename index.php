41208a65f18ee83c079d1715e775d92ccca89b4c
wangx6 <wangx6@gmail.com>
2013-12-26 17:36:04 +0000

    body tag corrected in index

--- index.php
+++ index.php
@@ -4,16 +4,9 @@
         <title>Git Demo First Project</title>
     </head>
     <body>
-    </body>
+    
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-echo "<h1>this is my first Git demo project</h2>";
+    echo "<h1>this is my first Git demo project</h2>";
 ?>
-
+        </body>
 </html>
