4754696e7c0eb9adf29cc684dfdce462a6c0d01a
wangx6 <wangx6@gmail.com>
2013-12-28 20:19:06 +0000

    classes are created [index.php]

--- index.php
+++ index.php
@@ -6,6 +6,43 @@
     <body>
     
 <?php
+    class Human
+    {
+        protected $age;
+	protected $name;
+	public function __construct($name, $age)
+	{
+	    this->name = $name;
+	    this->age = $age;
+	}
+	public function get_name()
+	{
+	    return this->name;
+	}
+	public function get_age()
+	{
+	    return this->age;
+	}
+    }
+
+    class Man extends Human
+    {
+        protected final $gender = "MALE";
+	public snore()
+	{
+	    return TURE;
+	}
+    }
+    
+    class Woman extends Human
+    {
+        protected final $gender = "FEMALE";
+	public giveBirth()
+	{
+	    return TURE;
+	}
+    }
+
     echo "<h2>this is my first Git demo project</h2>";
     echo "this is my test for branching and merging";
 ?>
