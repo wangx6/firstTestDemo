f609ce9f13d6eb4edb37b9dcbbf384f15f15f551
wangx6 <wangx6@gmail.com>
2013-12-28 21:03:36 +0000

    back to what it was [master/index.php]

--- index.php
+++ index.php
@@ -6,50 +6,8 @@
     <body>
     
 <?php
-<<<<<<< HEAD
-   echo 'hot fix is happening on this branch';
-   echo "<h2>this is my first Git demo project</h2>";
-=======
-    class Human
-    {
-        protected $age;
-	protected $name;
-	public function __construct($name, $age)
-	{
-	    this->name = $name;
-	    this->age = $age;
-	}
-	public function get_name()
-	{
-	    return this->name;
-	}
-	public function get_age()
-	{
-	    return this->age;
-	}
-    }
-
-    class Man extends Human
-    {
-        protected final $gender = "MALE";
-	public snore()
-	{
-	    return TURE;
-	}
-    }
-    
-    class Woman extends Human
-    {
-        protected final $gender = "FEMALE";
-	public giveBirth()
-	{
-	    return TURE;
-	}
-    }
-
     echo "<h2>this is my first Git demo project</h2>";
-    echo "this is my test for branching and merging";
->>>>>>> testing
+    echo "this is my test for branching and merging";:
 ?>
     </body>
 </html>
